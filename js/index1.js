fb3f7f364d231c34f4d30ab5eb665c2ba7d05d4b
sia <jwook486@gmail.com>
2023-02-27 01:35:37 +0900

    20230227

---
+++ js/index1.js
@@ -0,0 +1,171 @@
+
+$(document).ready(function () {
+    // typing animation
+    (function ($) {
+        $.fn.writeText = function (content) {
+            var contentArray = content.split(""),
+                current = 0,
+                elem = this;
+            setInterval(function () {
+                if (current < contentArray.length) {
+                    elem.text(elem.text() + contentArray[current++]);
+                }
+            }, 80);
+        };
+    })(jQuery);
+
+    // input text for typing animation
+    $("#holder").writeText("Web site design / published by sia");
+
+    // initialize wow.js
+    new WOW().init();
+
+    // Push the body and the nav over by 285px over
+    var main = function () {
+        $(".fa-bars").click(function () {
+            $(".nav-screen").animate(
+                {
+                    right: "0px"
+                },
+                200
+            );
+
+            $("body").animate(
+                {
+                    right: "285px"
+                },
+                200
+            );
+        });
+
+        // Then push them back */
+        $(".fa-times").click(function () {
+            $(".nav-screen").animate(
+                {
+                    right: "-285px"
+                },
+                200
+            );
+
+            $("body").animate(
+                {
+                    right: "0px"
+                },
+                200
+            );
+        });
+
+        $(".nav-links a").click(function () {
+            $(".nav-screen").animate(
+                {
+                    right: "-285px"
+                },
+                500
+            );
+
+            $("body").animate(
+                {
+                    right: "0px"
+                },
+                500
+            );
+        });
+    };
+
+    $(document).ready(main);
+
+    // initiate full page scroll
+
+    $("#fullpage").fullpage({
+        scrollBar: true,
+        responsiveWidth: 300,
+        navigation: true,
+        // navigationTooltips: ["home", "portfolio", "about",  "contact"],
+        // anchors: ["home", "portfolio", "about", "contact"],
+        menu: "#myMenu",
+        fitToSection: false,
+
+        afterLoad: function (anchorLink, index) {
+            var loadedSection = $(this);
+
+            //using index
+            if (index == 1) {
+                /* add opacity to arrow */
+                $(".xi-angle-down-thin").each(function () {
+                    $(this).css("opacity", "1");
+                });
+                $(".header-links a").each(function () {
+                    $(this).css("color", "white");
+                });
+                $(".header-links").css("background-color", "transparent");
+            } else if (index != 1) {
+                $(".header-links a").each(function () {
+                    $(this).css("color", "black");
+                });
+                // $(".header-links").css("background-color", "white");
+            }
+
+            //using index
+            // if (index == 2) {
+            //     /* animate skill bars */
+            //     $(".skillbar").each(function () {
+            //         $(this)
+            //             .find(".skillbar-bar")
+            //             .animate(
+            //                 {
+            //                     width: $(this).attr("data-percent")
+            //                 },
+            //                 2500
+            //             );
+            //     });
+            // }
+        }
+    });
+
+    // move section down one
+    $(document).on("click", "#moveDown", function () {
+        $.fn.fullpage.moveSectionDown();
+    });
+
+    // fullpage.js link navigation
+    $(document).on("click", "#portfolio", function () {
+        $.fn.fullpage.moveTo(2);
+    });
+
+    $(document).on("click", "#about", function () {
+        $.fn.fullpage.moveTo(3);
+    });
+
+    $(document).on("click", "#contact", function () {
+        $.fn.fullpage.moveTo(4);
+    });
+
+    // smooth scrolling
+    $(function () {
+        $("a[href*=#]:not([href=#])").click(function () {
+            if (
+                location.pathname.replace(/^\//, "") ==
+                this.pathname.replace(/^\//, "") &&
+                location.hostname == this.hostname
+            ) {
+                var target = $(this.hash);
+                target = target.length
+                    ? target
+                    : $("[name=" + this.hash.slice(1) + "]");
+                if (target.length) {
+                    $("html,body").animate(
+                        {
+                            scrollTop: target.offset().top
+                        },
+                        700
+                    );
+                    return false;
+                }
+            }
+        });
+    });
+
+    //ajax form
+
+});
+
